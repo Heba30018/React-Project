a85face7c8ce9c549ac53586dc53dea7c0b776cd
Heba <hiba30018@gmail.com>
2024-07-31 16:38:06 +0300

    Add Employee Design and Form Handling

--- EmployeeMS/src/components/Employee.jsx
+++ EmployeeMS/src/components/Employee.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 export const Employee = () => {
   return (
-    <div>Employee</div>
+   <div className='px-5 mt-3'>
+     <div className='d-flex justify-content-center'>
+      <h3>Employee List</h3>
+    </div>
+
+    <Link to="/dashboard/add_employee" 
+    className='btn btn-success'
+    >Add Employee</Link>
+
+
+<div className='mt-3'>
+      <table className='table'>
+       <thead>
+       <tr>
+          <th>Name</th>
+        </tr>
+       </thead>
+       <tbody>
+        
+       </tbody>
+      </table>
+    </div>
+
+   </div>
+   
   )
 }
+
