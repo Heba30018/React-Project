1623831f96144463f27f899eac303ba9b13456a0
Heba <hiba30018@gmail.com>
2024-07-31 15:51:00 +0300

    Display Category

--- EmployeeMS/src/components/Category.jsx
+++ EmployeeMS/src/components/Category.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 
 
 export const Category = () => {
+  const [category , setCategory] = useState([])
+
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/auth/category')
+    .then(result =>{
+        if(result.data.Status){
+          console.log(result.data)
+          setCategory(result.data.Result)
+        }else{
+          alert(result.data.Error)
+        }
+    }).catch(err => console.log(err))
+  },[])
+
+
   return (
     <div className='px-5 mt-3'>
       <div className='d-flex justify-content-center'>
@@ -12,6 +29,25 @@ export const Category = () => {
       <Link to="/dashboard/add_category" 
       className='btn btn-success'
       >Add Category</Link>
+
+      <div className='mt-3'>
+        <table className='table'>
+         <thead>
+         <tr>
+            <th>Name</th>
+          </tr>
+         </thead>
+         <tbody>
+            {
+              category.map(c =>(
+                <tr>
+                  <td>{c.name}</td>
+                </tr>
+              ))
+            }
+         </tbody>
+        </table>
+      </div>
 
     </div>
   )
